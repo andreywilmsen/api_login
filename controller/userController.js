a7cbc7e0fa8bba25258c98c60eb66bc0b0ecf65a
andreywilmsen <andreywilmsendepaula@gmail.com>
2024-04-04 09:23:39 -0300

    refactor: alter userController.js && routes.js && .env

--- controller/userController.js
+++ controller/userController.js
@@ -1,15 +1,31 @@
 require("dotenv").config();
 let User = require("../model/User");
+let bcrypt = require('bcrypt');
+let salt = Number(process.env.SALT);
 
 let controller = {
   createUser: async (req, res) => {
     let email = req.body.email;
-    let password = req.body.password;
+    let password = await bcrypt.hash(req.body.password, salt);
 
     let response = await User.create({ email, password });
     response.save();
 
     res.send("Usuário criado");
+  },
+  login: async (req, res) => {
+    let email = req.body.email;
+    let password = req.body.password;
+
+    let user = await User.findOne({ email });
+    if (!user) return res.status(404).send("User not found!");
+
+    let hash = user.password;
+    let validPassword = await bcrypt.compare(password, hash);
+
+    if (!validPassword) return res.status(404).send("Email or password wrong!")
+
+    res.send(validPassword);
   },
 };
 
--- routes/routes.js
+++ routes/routes.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const apiController = require("../controller/userController");
 
+router.get("/login", apiController.login);
+
 router.post("/createUser", apiController.createUser);
 
 module.exports = router;
