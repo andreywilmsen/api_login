fcfd48dbda18c5594a983b2a55fa25d34e7e833a
Andrey Wilmsen de Paula <71944788+andreywilmsen@users.noreply.github.com>
2024-04-07 12:55:09 -0300

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ const PORT = process.env.PORT;
 const router = require("./routes/routes");
 const db = require("./db/conn");
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Define o cabeçalho para permitir qualquer origem
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Define os métodos HTTP permitidos
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); // Define os cabeçalhos permitidos
+  next();
+});
+
 app.use("/", express.json(), router);
 
 app.listen(PORT, () => {
